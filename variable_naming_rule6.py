de8ac7048168304b6bed0e3d337e63eb071f70e1
Bhagesh.c <bhageshc51@gmail.com>
2026-02-15 23:20:43 +0530

    Add files via upload

---
+++ variable_naming_rule6.py
@@ -0,0 +1,10 @@
+name = "parasuram"  # string
+age = 25            # int
+is_student = False  # bool
+weight = 46.3      # float
+
+print(type(is_student))   
+
+is_student = "Yes"
+
+print(type(is_student))   
