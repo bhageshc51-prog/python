8ce6a89c1fe9c90c560d618561e128cd21fd854a
Bhagesh.c <bhageshc51@gmail.com>
2026-02-20 00:16:43 +0530

    Add files via upload

---
+++ day9-variable-naming-rule12.py
@@ -0,0 +1,21 @@
+name = "rahul"
+age = 22 # int
+is_student = True # bool
+weight = 60.75 # float
+
+print(type(is_student))
+
+s = "200"
+
+print(int(s) + age)
+
+a = 15
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
