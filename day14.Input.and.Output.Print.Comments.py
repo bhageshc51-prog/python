f0c5bbfb22b4f4a0bc415910e9e0f0fd8d345f17
Bhagesh.c <bhageshc51@gmail.com>
2026-02-26 23:54:40 +0530

    Add files via upload

---
+++ day14.Input.and.Output.Print.Comments.py
@@ -0,0 +1,11 @@
+print("karnataka")
+
+boy_name = input("Boy Name: ")
+boy_age = int(input("Boy Age: "))
+
+girl_name = input("Girl Name: ")
+girl_age = int(input("Girl Age: "))
+
+age_diff = boy_age - girl_age
+
+print(f"{boy_name} loves {girl_name}. Age Difference is {age_diff}")
