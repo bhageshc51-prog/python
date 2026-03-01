0095206facdbca5f6ca318ff3a93b1df748edcd9
Bhagesh.c <bhageshc51@gmail.com>
2026-03-01 23:55:27 +0530

    Add files via upload

---
+++ day16..String.Data.Type.Access.and.Manipulation2.py
@@ -0,0 +1,9 @@
+first_name = "bhagesh"
+last_name = "chandappa"
+full_name = first_name + " " + last_name
+
+message = "i love you"
+print(message*50)
+
+print(message.upper() )
+print(message.lower() )
