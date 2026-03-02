17d6e904b64358d2600be9f6a7f79f8a39eed095
Bhagesh.c <bhageshc51@gmail.com>
2026-03-03 00:36:07 +0530

    Add files via upload

---
+++ day19.acessing.string.carector2.py
@@ -0,0 +1,23 @@
+first_name = "bhagesh"
+last_name = "chandappa"
+full_name = first_name + " " + last_name
+
+message = "This is a Warning"
+print(message*50)
+
+print(message.upper() )
+print(message.lower() )
+print(message.strip() )
+print (message.replace("warning", "Error"))
+
+name = ''' bhagesh said "i love you" 
+        shivalila said " i love you 2 " 
+        '''
+        
+print(name) 
+
+print(len(message))
+
+name = "pallavi"  # length
+print(name[5]) #index =n position - 1
+print(name[3:6])
