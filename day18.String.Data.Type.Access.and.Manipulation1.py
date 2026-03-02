55668eabad54e499b5ec8452ec1a060d2827f625
Bhagesh.c <bhageshc51@gmail.com>
2026-03-02 23:23:09 +0530

    Add files via upload

---
+++ day18.String.Data.Type.Access.and.Manipulation1.py
@@ -0,0 +1,17 @@
+first_name = "bhagesh"
+last_name = "chandappa"
+full_name = first_name + " " + last_name
+
+message = "This is a Warning"
+print(message*50)
+
+print(message.upper() )
+print(message.lower() )
+print(message.strip() )
+print (message.replace("warning", "Error"))
+
+name = ''' bhagesh said "hello" 
+        narsing said "hi" 
+        '''
+        
+print(name) 
