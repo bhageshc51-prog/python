406d7165121d6106fc6db815e45a8d8b0b00a00a
Bhagesh.c <bhageshc51@gmail.com>
2026-03-03 00:36:52 +0530

    Add files via upload

---
+++ day19.acessing.string.carector4.py
@@ -0,0 +1,30 @@
+first_name = "bhagesh"
+last_name = "chandappa"
+full_name = first_name + " " + last_name
+
+message = "This is a Warning"
+print(message*50)
+
+print(message.upper() )
+print(message.lower() )
+print(message.strip() )
+print (message.replace("warning", "Error"))
+
+name = ''' nisarga said "what is yor name " 
+         bhagya said " my name is nisarga " 
+        '''
+        
+print(name) 
+
+print(len(message))
+
+name = "harshavardhan"  # length
+       
+print(name[-5]) #index =n position - 1
+print(name[:])
+print(name[-6])
+print(name[-4:-8])
+print(name[-1:])
+print(name[-2:])
+print(name[:12])
+print(name[-7:-13])
