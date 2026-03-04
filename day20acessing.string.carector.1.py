2d73a736c5bf9b26c8504d9795e30e3c92b42940
Bhagesh.c <bhageshc51@gmail.com>
2026-03-04 23:56:43 +0530

    Add files via upload

---
+++ day20acessing.string.carector.1.py
@@ -0,0 +1,35 @@
+first_name = "Rahul"
+last_name = "Sharma"
+full_name = first_name + " " + last_name
+
+message = "This is a Warning"
+print(message * 5)
+
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("Warning", "Error"))
+
+name = """Anu said "What is your name?"
+Ravi said "My name is Anu"
+"""
+
+print(name)
+
+print(len(message))
+
+name = "suryaprakash"
+
+print(name[-5])
+print(name[:])
+print(name[-6])
+print(name[-8:-4])
+print(name[-1:])
+print(name[-2:])
+print(name[:10])
+print(name[-10:-5])
+
+print(name[::3])
+print(name[::-1])
+print(name[::-2])
+print(name[::2])
