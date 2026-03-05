63540830b5a655ce3aeb40cfedee710987339a00
Bhagesh.c <bhageshc51@gmail.com>
2026-03-05 22:18:02 +0530

    Add files via upload

---
+++ day21.acessing.string.carector1.py
@@ -0,0 +1,38 @@
+first_name = "lakshman"
+last_name = "golgappa"
+full_name = first_name + " " + last_name
+
+message = "how are you man "
+print(message*50)
+
+print(message.upper() )
+print(message.lower() )
+print(message.strip() )
+print (message.replace("warning", "Error"))
+
+name = ''' manik  said "what is yor name " 
+         sahana said " my name is sahana " 
+        '''
+        
+print(name) 
+
+print(len(message))
+
+name = "govardhanrayking"  # length
+       
+print(name[-5]) #index =n position - 1
+print(name[:])
+print(name[-6])
+print(name[-4:-8])
+print(name[-1:])
+print(name[-2:])
+print(name[:12])
+print(name[-7:-13])
+
+print(name[::3])
+print(name[::-1])
+print(name[::-2])
+print(name[::2])
+
+print(name[1:2:3])
+
