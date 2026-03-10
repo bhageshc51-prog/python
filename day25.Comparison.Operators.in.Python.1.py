051eadc9887bf1c90fd227be76a71ee806d5d6cc
Bhagesh.c <bhageshc51@gmail.com>
2026-03-10 21:22:20 +0530

    Add files via upload

---
+++ day25.Comparison.Operators.in.Python.1.py
@@ -0,0 +1,11 @@
+# Comparison Operators in Python
+
+a = 2536
+b = 6047
+
+print("a < b  :", a < b)
+print("a > b  :", a > b)
+print("a <= b :", a <= b)
+print("a >= b :", a >= b)
+print("a == b :", a == b)
+print("a != b :", a != b)
