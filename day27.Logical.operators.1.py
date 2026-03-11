ef16de21324eb66c36331b37205503b9c1d7ae95
Bhagesh.c <bhageshc51@gmail.com>
2026-03-12 01:49:39 +0530

    Add files via upload

---
+++ day27.Logical.operators.1.py
@@ -0,0 +1,19 @@
+# Operators
+
+print("AND Operator")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("\nOR Operator")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("\nNOT Operator")
+print(not True)
+print(not False)
+print(not(False))
+
