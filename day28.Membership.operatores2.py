9ddd55344fc07e16df67890b9f1a3e963245d4e6
Bhagesh.c <bhageshc51@gmail.com>
2026-03-13 00:04:56 +0530

    Add files via upload

---
+++ day28.Membership.operatores2.py
@@ -0,0 +1,10 @@
+# Operators (Membership + Logical)
+
+s = "Bhagesh"        # string variable storing "Bhagesh"
+S2 = "Bhagesh.c"     # string variable storing "Bhagesh.c"
+
+# Checks membership using AND operator
+print(("a" in s) and ("s" in S2))
+
+# Checks membership using OR operator
+print(("e" in s) or ("h" in S2))
