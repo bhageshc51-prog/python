17c643a87d6b492ffa86a10b91608b41a1a68ed2
Bhagesh.c <bhageshc51@gmail.com>
2026-03-25 01:04:20 +0530

    Add files via upload

---
+++ day29.Lists.in.python2.py
@@ -0,0 +1,13 @@
+# lists 
+
+items = ["bru", "sting", "ground nut", "pen", "Book"]
+
+print(items[0]) 
+print(items[1]) 
+print(items[2]) 
+print(items[3]) 
+print(items[4]) 
+print(items[-1]) 
+print(items[-2]) 
+print(items[-3]) 
+print(items[-4]) 
