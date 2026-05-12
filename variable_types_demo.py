f8ef012c58a59f082d9821e35c01aea5d762dc32
Bhagesh.c <bhageshc51@gmail.com>
2026-05-12 22:03:33 +0530

    Added simple Python code to check variable data types
    This is a simple Python program that uses different types of variables such as string, integer, boolean, and float. It also checks the datatype of variables using the type() function. This program is useful for beginners to understand basic Python concepts and variable usage.

---
+++ variable_types_demo.py
@@ -0,0 +1,11 @@
+# File Name: variable_types_demo.py
+
+student_name = "Deepak"
+marks = 88
+passed = True      # boolean
+percentage = 87.4  # float
+
+print(type(marks))
+
+value = "500"
+print(type(value))
