fc00f2403a2d40336d6b97568728c3d0fe554432
Bhagesh.c <bhageshc51@gmail.com>
2026-05-16 22:48:44 +0530

    Remove comments and clean up variable declarations

--- variable_types_demo.py
+++ variable_types_demo.py
@@ -1,9 +1,7 @@
-# File Name: variable_types_demo.py
-
 student_name = "Deepak"
 marks = 88
-passed = True      # boolean
-percentage = 87.4  # float
+passed = True      
+percentage = 87.4  
 
 print(type(marks))
 
