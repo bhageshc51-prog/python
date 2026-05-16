7e14adca1b4f454b8c1befff5782194f68c10349
Bhagesh.c <bhageshc51@gmail.com>
2026-05-16 23:18:08 +0530

    Added variables and print statement program
    Description:
    This Python program shows how to use variables and the print() statement. It stores values in variables and prints a string using double quotes.

--- 1.py
+++ 1.py
@@ -2,3 +2,12 @@ a = 10
 b = 20
 
 print("a")
+
+# Output:
+a
+
+# Explanation:
+1.In this program, two variables named a and b are created.
+2.Values 10 and 20 are stored in the variables.
+3.The print("a") statement prints only the letter a.
+4.This happens because a is written inside double quotes.
