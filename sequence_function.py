30f18ca95a9a70507777ea94deb00044cfd34b21
Bhagesh.c <bhageshc51@gmail.com>
2026-05-18 10:52:41 +0530

    Add files via upload

---
+++ sequence_function.py
@@ -0,0 +1,6 @@
+def sequence(n):
+    for i in range(1, n + 1):
+        print(i, end=" ")
+
+num = int(input("Enter a number: "))
+sequence(num)
---
+++ sequence_lambda.py
@@ -0,0 +1,4 @@
+sequence = lambda n: [print(i, end=" ") for i in range(1, n + 1)]
+
+num = int(input("Enter a number: "))
+sequence(num)
