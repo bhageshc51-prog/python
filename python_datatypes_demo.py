0a1cf77931583f4f56da12bf96c62d907f74507e
Bhagesh.c <bhageshc51@gmail.com>
2026-05-21 21:35:28 +0530

    Added Python program demonstrating basic datatypes
    This Python program demonstrates the use of basic datatypes such as string, integer, boolean, and float. It also uses the type() function to identify and display the datatype of different variables entered by the user.

---
+++ python_datatypes_demo.py
@@ -0,0 +1,12 @@
+student_name = input("Enter student name: ")
+marks = int(input("Enter marks: "))
+
+percentage = float(input("Enter percentage: "))
+
+print("Student Name:", student_name)
+print("Marks:", marks)
+print("Percentage:", percentage)
+
+print(type(student_name))
+print(type(marks))
+print(type(percentage))
