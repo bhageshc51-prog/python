7caba8f7eab7fb0f8d7254a394c1d04b17b1d519
Bhagesh.c <bhageshc51@gmail.com>
2026-05-22 21:29:03 +0530

    Added Python type conversion examples
    This program demonstrates type conversion in Python using int(), float(), and str() functions. It converts data between different data types and checks the datatype using type().

---
+++ type_conversion.py
@@ -0,0 +1,27 @@
+# Type Conversion in Python
+
+num1 = "100"
+num2 = "50"
+
+# Converting string to integer
+sum = int(num1) + int(num2)
+
+print("Addition :", sum)
+
+# Integer to float
+marks = 85
+percentage = float(marks)
+
+print("Percentage :", percentage)
+
+# Float to integer
+value = 45.89
+new_value = int(value)
+
+print("Converted Value :", new_value)
+
+# Integer to string
+number = 500
+text = str(number)
+
+print(type(text))
