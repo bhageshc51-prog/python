cf1110005a87d4e0435e6ea3fff25b0c6ef40d28
Bhagesh.c <bhageshc51@gmail.com>
2026-05-30 09:29:46 +0530

    Add type conversion examples for marks and percentage

---
+++ type_conversion2.py
@@ -0,0 +1,15 @@
+student_name = "Deepak"
+marks = "88"
+
+print("Before Conversion:", type(marks))
+
+marks = int(marks)
+
+print("After Conversion:", type(marks))
+print("Marks + 10 =", marks + 10)
+
+percentage = 87.4
+percentage_int = int(percentage)
+
+print("Original Percentage:", percentage)
+print("Converted Percentage:", percentage_int)
