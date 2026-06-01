e36ddaf4de5e697fbb8a7059c66f9cc10fc8015f
Bhagesh.c <bhageshc51@gmail.com>
2026-06-01 19:33:16 +0530

    Add employee variables and print their types

---
+++ 01_variables_and_datatypes.py
@@ -0,0 +1,12 @@
+employee_name = "Rahul"
+age = 25
+is_working = True
+salary = 35000.50
+
+print(type(employee_name))
+print(type(age))
+print(type(is_working))
+print(type(salary))
+
+city = "Bengaluru"
+print(type(city))
