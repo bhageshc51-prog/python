74cdbf6af5556317c4c0141e32708825bb31c096
Bhagesh.c <bhageshc51@gmail.com>
2026-06-03 11:04:57 -0400

    Add program demonstrating type casting in Python
    This program demonstrates type casting in Python by converting string values into integer and float data types using int() and float() functions.

---
+++ type_casting.py
@@ -0,0 +1,14 @@
+student_name = "Deepak"
+marks = "88"
+percentage = "87.4"
+
+marks = int(marks)
+percentage = float(percentage)
+
+print("Student Name:", student_name)
+print("Marks:", marks)
+print("Percentage:", percentage)
+
+print(type(student_name))
+print(type(marks))
+print(type(percentage))
