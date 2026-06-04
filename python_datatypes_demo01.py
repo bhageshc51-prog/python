39142465780d34a137f550c323d9322bc720aec4
Bhagesh.c <bhageshc51@gmail.com>
2026-06-04 13:09:36 -0400

    Add variables, data types, and type() function examples
    This program demonstrates Python variables and basic data types including string, integer, float, and boolean. It also shows how to use the type() function to identify the data type of a variable.

---
+++ python_datatypes_demo01.py
@@ -0,0 +1,36 @@
+student_name = "Deepak"
+marks = 88
+passed = True
+percentage = 87.4
+
+print(type(marks))
+
+value = "500"
+print(type(value))
+
+
+#Output
+<class 'int'>
+<class 'str'>
+
+#Explanation
+# Creating a string variable to store the student's name
+student_name = "Deepak"
+
+# Creating an integer variable to store marks
+marks = 88
+
+# Creating a boolean variable to indicate pass/fail status
+passed = True
+
+# Creating a float variable to store percentage
+percentage = 87.4
+
+# Printing the data type of the marks variable
+print(type(marks))
+
+# Creating a string variable containing the characters "500"
+value = "500"
+
+# Printing the data type of value
+print(type(value))
