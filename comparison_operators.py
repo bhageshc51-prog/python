c5bb5687b2993a58275458e4d51c05a30ba1cd3d
Bhagesh.c <bhageshc51@gmail.com>
2026-06-11 22:23:57 +0530

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,11 @@
+# Comparison Operators
+
+a = 15
+b = 10
+
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
